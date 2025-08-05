69c488b654d71ff250f6f6bd577e6a9c102d8f7c
Mostafa Alaaeldin <mostafa.alaaeldin@codezone-eg.com>
2025-08-05 10:53:09 +0300

    Finished Delete action

--- Day2/ProductsComponents/Products.razor.cs
+++ Day2/ProductsComponents/Products.razor.cs
@@ -6,6 +6,9 @@
 
         public int NewProductID { get; set; }
 
+        public int ProducIDToBeDeleted { get; set; }
+        public string ProductNameToBeDeleted { get; set; } = string.Empty;
+
         protected override void OnInitialized()
         {
             products = ProductsServices?.GetAll()?.ToList() ?? new List<Product>();
@@ -15,6 +18,18 @@
         void GetNewID()
         {
             NewProductID = ProductsServices.GetNextNewProductID();
+        }
+
+        void GetProductNameThatWillBeDeleted(int id)
+        {
+            ProducIDToBeDeleted = id;
+            ProductNameToBeDeleted = ProductsServices?.GetByID(id)?.Name ?? string.Empty;
+        }
+
+        void DeleteProduct()
+        {
+            ProductsServices.Delete(ProducIDToBeDeleted);
+            products = ProductsServices?.GetAll()?.ToList() ?? new List<Product>();
         }
     }
 }
